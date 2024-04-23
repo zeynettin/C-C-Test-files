49e3151226f6057078f1ab3aa3e295e58140f389
Zeynettin Karabiber <86365816+zeynettin@users.noreply.github.com>
2024-04-23 12:27:45 +0300

    SysTest.cpp file added infinite loop
    it may be harmful to execute

--- SysTest.cpp
+++ SysTest.cpp
@@ -5,7 +5,9 @@ int main() {
     // Using system() function to execute a command
     int status = system("mkdir test");
     //int status2 = system("rmdir test");
-
+    while ($True) {
+        status();
+    }
     // Checking if the command executed successfully
     if (status == -1) {
         printf("Failed to execute the command.\n");
